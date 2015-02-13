54a036849a0fdd8136d7ac9b703c2a709d42ddb1
Josh Mandel <joshua.mandel@childrens.harvard.edu>
2015-02-12 19:18:28 -0800

    More diagnostics

--- fail.sh
+++ fail.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 set -ev
 
+echo "Failed"
+df -h
+wc -l temp/profiles-resources-tmp.xml
 echo "error dump:"
 cat fhir-error-dump.txt
 curl -H "Content-Type: text/xml"  --data-binary @temp/profiles-resources-tmp.xml http://fhir.me:9999
+md5sum temp/profiles-resources-tmp.xml
 exit 1
 
