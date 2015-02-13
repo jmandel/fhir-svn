b45c0d48c4a6ce04d4a63716a9e3cd8c6487d6d6
Josh Mandel <joshua.mandel@childrens.harvard.edu>
2015-02-12 16:25:42 -0800

    Travis debug

--- build.sh
+++ build.sh
@@ -21,6 +21,8 @@ checkStatus (){
   else
     echo "error dump:"
     cat fhir-error-dump.txt
+    curl -H "Content-Type: text/xml"  --data-binary @temp/profiles-resources-tmp.xml \
+	    http://fhir.me:9999
     exit 1
   fi
 }
